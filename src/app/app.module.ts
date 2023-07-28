cbf8427efa7a915c21f5df558ab5e180ff9b5184
alex.karamushko <aleksandrkaramusko@Air-Aleksandr.nordclan>
2023-07-28 17:20:02 +0400

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
+import { MainComponent } from '../pages/main/main.component';
 import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [
+  {
+    component: MainComponent,
+    path: '',
+  },
+];
 
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent],
   imports: [
-    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
 })
 
