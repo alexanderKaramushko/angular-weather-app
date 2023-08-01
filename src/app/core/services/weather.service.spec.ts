a1d64d5a1db73547316c4d18febc1f0a91e0a67b
alex.karamushko <aleksandrkaramusko@Air-Aleksandr.nordclan>
2023-08-01 17:14:43 +0400

    test WeatherService for errors

--- src/app/core/services/weather.service.spec.ts
+++ src/app/core/services/weather.service.spec.ts
@@ -1,4 +1,5 @@
 /* eslint-disable max-classes-per-file */
+import { HttpErrorResponse } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
@@ -24,7 +25,9 @@ describe('WeatherService', () => {
 
   it('should be created', () => {
     const forecast = new Forecast(55, 55, {
-      temperature_2m: [10, 20, 30] as any[],
+      // todo fix any
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      temperature_2m: [10, 20, 30] as any,
     });
 
     service
@@ -42,5 +45,31 @@ describe('WeatherService', () => {
     expect(req.request.method).toBe('GET');
 
     req.flush(forecast);
+  });
+
+  it('should throw error if request error', () => {
+    const message = 'Not found';
+
+    service
+      .getForecast({
+        latitude: 55,
+        longitude: 55,
+      })
+      .subscribe({
+        error: (error: HttpErrorResponse) => {
+          expect(error.status).withContext('status').toEqual(404);
+          expect(error.error).withContext('message').toEqual(message);
+        },
+        next: () => fail('should have failed with the 404 error'),
+      });
+
+    const req = httpTestingController.expectOne('/forecast?latitude=55&longitude=55');
+
+    expect(req.request.method).toBe('GET');
+
+    req.flush(message, {
+      status: 404,
+      statusText: 'Not found',
+    });
   });
 });
