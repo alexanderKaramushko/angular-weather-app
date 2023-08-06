0fe88e3d2384c803d2eb940bd9cad066751da6cf
alex.karamushko <aleksandrkaramusko@Air-Aleksandr.nordclan>
2023-08-06 20:27:57 +0400

    add zone flags to skip change detections

---
+++ src/zone-flags.ts
@@ -0,0 +1,5 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+(window as any).__Zone_disable_requestAnimationFrame = true;
+
+(window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove'];
