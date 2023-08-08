0908990030f9c49955c736f9d8296c5138855c4a
alex.karamushko <aleksandrkaramusko@Air-Aleksandr.nordclan>
2023-08-08 17:40:13 +0400

    autocancel source forecast stream

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,44 +1,30 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { AfterViewInit, Component } from '@angular/core';
+import { debounceTime, Subject, switchMap } from 'rxjs';
 
-import { Forecast } from 'src/app/core/models/forecast.model';
-import { WeatherService } from 'src/app/core/services/weather.service';
+import { ForecastQuery, WeatherService } from 'src/app/core/services/weather.service';
 
 @Component({
   selector: 'main',
   styleUrls: ['./main.component.css'],
   templateUrl: './main.component.html',
 })
-export class MainComponent implements OnInit, OnDestroy {
+export class MainComponent implements AfterViewInit {
 
-  forecast$: Observable<Forecast> | undefined;
-  forecastSubscription: Subscription | undefined;
-  isLoading = false;
+  forecast$ = new Subject();
+
+  getForecast$ = this.forecast$.pipe(
+    debounceTime(250),
+    switchMap((query: ForecastQuery) => this.weather.getForecast(query)),
+  )
 
   constructor(private weather: WeatherService) { }
 
-  ngOnInit(): void {
-    this.isLoading = true;
-
-    this.forecast$ = this.weather
-      .getForecast({
-        currentWeather: true,
-        latitude: 37.625,
-        longitude: 55.75,
-      });
-
-    this.forecastSubscription = this.forecast$
-      .subscribe({
-        complete: () => {
-          this.isLoading = false;
-        },
-      });
-  }
-
-  ngOnDestroy(): void {
-    if (this.forecastSubscription) {
-      this.forecastSubscription.unsubscribe();
-    }
+  ngAfterViewInit(): void {
+    this.forecast$.next({
+      currentWeather: true,
+      latitude: 37.625,
+      longitude: 55.75,
+    });
   }
 
 }
