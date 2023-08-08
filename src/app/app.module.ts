b379eac7458e328527bdcc8c6a1f2e599cc3d645
alex.karamushko <aleksandrkaramusko@Air-Aleksandr.nordclan>
2023-08-08 14:53:54 +0400

    add layouts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { environment } from 'src/environments/environment';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { httpInterceptorProviders } from './core/interceptors/httpInterceptorProviders';
+import { HeaderComponent } from './core/layout/header/header.component';
+import { MainComponent } from './core/layout/main/main.component';
 import { ENVIRONMENT } from './core/services/environment.service';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   bootstrap: [AppComponent],
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, MainComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
---
+++ src/app/core/layout/main/main.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-layout',
+  styleUrls: ['./main.component.css'],
+  templateUrl: './main.component.html',
+})
+export class MainComponent {
+
+}
