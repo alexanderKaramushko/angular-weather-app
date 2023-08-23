a6ff82acce5a3f83d144cfc0d7eaf3d28bd05c3d
alex.karamushko <aleksandrkaramusko@Air-Aleksandr.nordclan>
2023-08-23 18:37:51 +0400

    add theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,13 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { NotificationsService, NotificationType } from './core/services/notifications.service';
 import { CustomSnackbarComponent, CustomSnackbarData, CustomSnackbarType } from './shared/components/custom-snackbar/custom-snackbar.component';
 
+enum Theme {
+  DARK = 'black-theme',
+  LIGHT = 'light-theme',
+  NATURE = 'nature-theme',
+  DEFAULT = 'default-theme',
+}
+
 @Component({
   selector: 'app-root',
   styleUrls: ['./app.component.css'],
@@ -11,24 +18,30 @@ import { CustomSnackbarComponent, CustomSnackbarData, CustomSnackbarType } from 
 })
 export class AppComponent implements OnInit {
 
-  constructor(public notifications: NotificationsService, private _snackBar: MatSnackBar) { }
+  constructor(public notifications: NotificationsService, private snackBar: MatSnackBar) { }
 
   snackbarTypes: Record<number, CustomSnackbarType> = {
     [NotificationType.ERROR]: CustomSnackbarType.ERROR,
     [NotificationType.SUCCESS]: CustomSnackbarType.SUCCESS,
   };
 
+  // todo вынести управление настройками в feature
+  theme = Theme.LIGHT;
+
+  themes = Theme;
+
   ngOnInit(): void {
     this.notifications.notifications$.subscribe((notifications) => {
       const [notification] = notifications;
 
       if (notification) {
-        // eslint-disable-next-line no-underscore-dangle
-        this._snackBar.openFromComponent<CustomSnackbarComponent, CustomSnackbarData>(CustomSnackbarComponent, {
+        // todo вынести управление показом в сервис
+        this.snackBar.openFromComponent<CustomSnackbarComponent, CustomSnackbarData>(CustomSnackbarComponent, {
           data: {
             message: notification.message,
             type: this.snackbarTypes[notification.type],
           },
+          panelClass: 'notification-overlay',
         });
       }
     });
