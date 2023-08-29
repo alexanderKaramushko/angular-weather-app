d7e8ac9bcb40fdccaa26ee0e96d753d17220dbe7
alex.karamushko <aleksandrkaramusko@Air-Aleksandr.nordclan>
2023-08-29 11:22:28 +0400

    move utils into shared

--- src/app/shared/components/if-loaded-view/if-loaded-view.module.ts
+++ src/app/shared/components/if-loaded-view/if-loaded-view.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
-import { IfLoadedDirective } from 'src/app/utils/directives/if-loaded.directive';
+import { IfLoadedDirective } from 'src/app/shared/directives/if-loaded.directive';
 
 import { IfLoadedViewComponent } from './if-loaded-view.component';
 
